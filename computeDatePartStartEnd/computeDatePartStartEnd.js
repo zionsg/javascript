53f2af511c719cf41e457db32726286e042d2389
Ng Kitt Horng, Zion <zion@intzone.com>
2023-04-24 13:43:44 +0800

    Add notes on DST

--- computeDatePartStartEnd/computeDatePartStartEnd.js
+++ computeDatePartStartEnd/computeDatePartStartEnd.js
@@ -1,22 +1,30 @@
 /**
  * Compute start/end of date part for specified date
  *
+ * Factor in differences in timezone offset for the same region due to
+ * Daylight Saving Time (DST) when running this method on a timestamp
+ * from summer and validating the answer via third party tools such as
+ * https://www.epochconverter.com/ during winter, and vice versa. See notes
+ * on DST at
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTimezoneOffset
+ * for more info.
+ *
  * @example Given timestamp of 1677609045123
  *      (Tue 28 Feb 2023 18:30:45.123 UTC, Wed 01 Mar 2023 02:30:45.123 SGT),
  *      this method will yield the following:
  *      - When run in JavaScript on a browser in Singapore timezone +08:00
- *        (timestamp, 'year', 'start'):  Sun 01 Jan 2023 0:00:00.000 SGT
+ *        (timestamp, 'year', 'start'):  Sun 01 Jan 2023 00:00:00.000 SGT
  *        (timestamp, 'year', 'end'):    Sun 31 Dec 2023 23:59:59.999 SGT
- *        (timestamp, 'month', 'start'): Wed 01 Mar 2023 0:00:00.000 SGT
+ *        (timestamp, 'month', 'start'): Wed 01 Mar 2023 00:00:00.000 SGT
  *        (timestamp, 'month', 'end'):   Fri 31 Mar 2023 23:59:59.999 SGT
- *        (timestamp, 'day', 'start'):   Wed 01 Mar 2023 0:00:00.000 SGT
+ *        (timestamp, 'day', 'start'):   Wed 01 Mar 2023 00:00:00.000 SGT
  *        (timestamp, 'day', 'end'):     Wed 01 Mar 2023 23:59:59.999 SGT
  *      - When run in Node.js on a server set to UTC timezone +00:00
  *        (timestamp, 'year', 'start'):  Sun 01 Jan 2023 00:00:00.000 UTC
  *        (timestamp, 'year', 'end'):    Sun 31 Dec 2023 23:59:59.999 UTC
- *        (timestamp, 'month', 'start'): Wed 01 Feb 2023 0:00:00.000 UTC
+ *        (timestamp, 'month', 'start'): Wed 01 Feb 2023 00:00:00.000 UTC
  *        (timestamp, 'month', 'end'):   Tue 28 Feb 2023 23:59:59.999 UTC
- *        (timestamp, 'day', 'start'):   Tue 28 Feb 2023 0:00:00.000 UTC
+ *        (timestamp, 'day', 'start'):   Tue 28 Feb 2023 00:00:00.000 UTC
  *        (timestamp, 'day', 'end'):     Tue 28 Feb 2023 23:59:59.999 UTC
  * @param {int} timestampMs - UNIX timestamp in milliseconds.
  * @param {string="year","month","day"} datePart - Date part to compute.
