5d38af00d9c9ed64e5fbb97393ca98557c891fd9
Zion Ng <zion@intzone.com>
2024-12-10 15:15:08 +0800

    Use exponential backoff with jitter

--- poll/poll.js
+++ poll/poll.js
@@ -6,71 +6,82 @@
  * dependent on it.
  *
  * Note that the poll function is not executed immediately on invocation
- * of this method.
+ * of this method as it is assumed that it will not return true that fast.
+ *
+ * Exponential backoff with Full Jitter algorithm used as per
+ * https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+ * which does not need to keep track of previous interval as in the case
+ * of Decorrelated Jitter.
  *
  * @public
  * @link https://en.wikipedia.org/wiki/Exponential_backoff
  * @link https://exponentialbackoffcalculator.com/
  * @param {function(): Promise<boolean>} pollFunction - Async function
  *     to execute for polling. The polling stops when the function returns
- *     true or when the maximum retries is reached.
- * @param {int} maximumAttempts=-1 - Maximum no. of polling attempts. Set
+ *     true or when `maxAttempts` is reached.
+ * @param {int} maxAttempts=-1 - Maximum no. of polling attempts. Set
  *     to -1 for infinite no. of attempts.
- * @param {int} intervalMs=1000 - No. of milliseconds between each attempt.
- * @param {boolean} exponentialBackoffFactor=2 - Multiplicative factor
- *     applied on `intervalMs` with each attempt. E.g. if intervalMs=1000
- *     and this value is 2, the 1st attempt is made 1 second after
- *     this method is invoked, the 2nd attempt is made 2 seconds after
- *     the 1st attempt, 3rd attempt 4 secs later, 4th attempt 8 secs later,
- *     and so forth.
+ * @param {int} baseIntervalMs=1000 - No. of milliseconds from method
+ *     invocation to 1st attempt, used as a base for computing intervals for
+ *     subsequent attempts.
+ * @param {int} maxIntervalMs=8000 - Maximum no. of milliseconds to wait
+ *     till next attempt. On average, the no. of milliseconds between each
+ *     attempt will be approximately half of this value.
  * @returns {Promise<void>}
- * @throws {Error} if invalid poll function, maximum attempts exceeded,
- *     interval too long, etc.
+ * @throws {Error} if invalid poll function, error executing poll function,
+ *     or maximum attempts exceeded.
  */
 async function poll(
     pollFunction,
-    maximumAttempts = -1,
-    intervalMs = 1000,
-    exponentialBackoffFactor = 2
+    maxAttempts = -1,
+    baseIntervalMs = 1000,
+    maxIntervalMs = 8000
 ) {
     if (typeof pollFunction !== 'function') {
-        throw new Error('Poll function invalid.');
+        throw new Error('Invalid poll function.');
     }
 
     let attemptFunction = async function (resolveFunction, rejectFunction, attempts = 0) {
-        if (maximumAttempts > 0 && attempts >= maximumAttempts) {
-            rejectFunction(`Poll exceeded maximum attempts of ${maximumAttempts}.`);
+        if (maxAttempts > 0 && attempts >= maxAttempts) {
+            rejectFunction(`Poll exceeded maximum attempts of ${maxAttempts}.`);
             return;
         }
 
         // Insist on boolean to avoid poll functions mistakenly returning API responses
-        let pollResult = await pollFunction();
+        let pollResult = false;
+        try {
+            pollResult = await pollFunction();
+        } catch (err) {
+            rejectFunction(`Error executing poll function: ${err.message}.`);
+            return;
+        }
         if (true === pollResult) {
             resolveFunction();
             return;
         }
 
         let newAttemptCnt = attempts + 1;
-        let newIntervalMs = Math.pow(exponentialBackoffFactor, newAttemptCnt) * intervalMs;
+        let newIntervalMs = Math.min(maxIntervalMs, Math.pow(2, newAttemptCnt) * baseIntervalMs);
         if (!Number.isFinite(newIntervalMs)) { // ensure not NaN and not Infinity (e.g. 1024^1024)
-            rejectFunction(`Poll interval too large after ${newAttemptCnt} attempts.`);
-            return;
+            newIntervalMs = maxIntervalMs;
         }
 
+        let newIntervalMsWithJitter = Math.floor(Math.random() * newIntervalMs);
         setTimeout(
             async function () {
                 await attemptFunction(resolveFunction, rejectFunction, newAttemptCnt);
             },
-            newIntervalMs
+            newIntervalMsWithJitter
         );
     };
 
+    // Delayed execution for 1st attempt
     return new Promise((resolve, reject) => {
         setTimeout(
             async function () {
                 await attemptFunction(resolve, reject);
             },
-            intervalMs
+            baseIntervalMs
         );
     });
 }
