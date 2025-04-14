ce24fb4f7c00ee7b974411d7c180a31d3c517a34
Ng Kitt Horng, Zion <zion@intzone.com>
2025-04-14 14:05:45 +0800

    Update sanitizeText.js

--- sanitizeText/sanitizeText.js
+++ sanitizeText/sanitizeText.js
@@ -6,17 +6,20 @@
  * @returns {string}
  */
 function sanitizeText(text) {
-    let result = (text ?? '').toString().trim(); // trim text
+    let result = (text ?? '').toString();
     if (!result.length) {
         return result;
     }
 
-    // Remove \r, replace tabs with single spaces, reduce multiple consecutive spaces to a single space
-    result = result.replace(/\r/g, '').replace(/\t/g, ' ').replace(/ {2,}/g, ' ');
+    // Replace with single space: \r, tab, no-break space, en space, em space, zero width space
+    result = result.replace(/[\r\t\u00A0\u2002\u2003\u200B]/g, ' ');
+
+    // Reduce multiple consecutive spaces (not whitespace \s which includes newline) to a single space
+    result = result.replace(/ {2,}/g, ' ');
 
     // Replace smart punctuation with normal ASCII forms
     // See https://www.w3.org/wiki/Common_HTML_entities_used_for_typography
-    result = result.replace(/[“”″]/g, '"').replace(/[‘’′]/g, "'").replace(/[—–]/g, '-');
-
-    return result;
+    result = result.replace(/[\u201C\u201D\u2033]/g, '"') // “ ” ″
+        .replace(/[\u2018\u2019\u2032]/g, "'") // ‘ ’ ′
+        .replace(/[\u2010\u2013\u2014]/g, '-'); // ‐ — –
 }
