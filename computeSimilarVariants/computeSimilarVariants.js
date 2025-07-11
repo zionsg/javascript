07aeab972ed1032a748032d7d0fccd1e4730ae92
Zion Ng <zion@intzone.com>
2025-07-11 11:30:37 +0800

    Add computeSimilarVariants

---
+++ computeSimilarVariants/computeSimilarVariants.js
@@ -0,0 +1,66 @@
+/**
+ * Generate list of similar variants for input text
+ *
+ * @example Given "D@BS" as input, results will be:
+ *     ['DBS', 'DBS', 'DB5', 'D8S', 'D85', '0BS', '0B5', '08S', '085']
+ * @param {string} input
+ * @returns {string[]} Symbols will be removed, digits will be replaced by
+ *     similar looking letters, and all letters will be uppercased.
+ */
+function computeSimilarVariants(input) {
+    // Map digits to similar looking letters
+    let digitLetterMap = {
+        '0': ['D', 'O'],
+        '1': ['I', 'l'], // capital i and small L
+        '2': ['Z'],
+        '3': ['E'], // mirror
+        '4': ['h'], // 4 written without the slant looks like inverted "h"
+        '5': ['S'],
+        '6': ['G'],
+        '7': ['J', 'T'],
+        '8': ['B'],
+        '9': ['g'],
+    };
+
+    // Standardize to uppercase for all letters and construct reverse map
+    let letterDigitMap = {};
+    Object.keys(digitLetterMap).forEach((digit) => {
+        digitLetterMap[digit].forEach((letter, letterIndex) => {
+            letter = letter.toUpperCase();
+            digitLetterMap[digit][letterIndex] = letter;
+
+            if (!letterDigitMap[letter]) {
+                letterDigitMap[letter] = [];
+            }
+
+            letterDigitMap[letter].push(digit.toString());
+        });
+    });
+
+    let normalizedText = input.toUpperCase().replace(/[^a-z0-9]/gi, '');
+    let results = ['']; // empty string as starting result
+    normalizedText.split('').forEach((character) => {
+        let similarChars = [];
+        if (character == parseInt(character)) { // digit
+            similarChars = digitLetterMap[character] ?? [];
+        } else { // letter
+            similarChars = letterDigitMap[character] ?? [];
+        }
+
+        let newResults = [];
+        results.forEach((partialResult) => {
+            newResults.push(partialResult + character);
+
+            similarChars.forEach((similarChar) => {
+                newResults.push(partialResult + similarChar);
+            });
+        });
+
+        // Override
+        results = newResults;
+    });
+
+    results.unshift(normalizedText);
+
+    return results;
+}
